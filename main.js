3230a03e3b6aff05da507188eefc4f294594bdae
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-03-31 02:02:12 +0530

    Done

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
   let slides = document.getElementsByClassName("slide");
-  for (let i = 0; i < slides.length; i++) {
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) { slideIndex = 1 }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+let prevBtn = document.getElementById("prevBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+prevBtn.addEventListener("click", function() {
+  plusSlides(-1);
+});
+
+nextBtn.addEventListener("click", function() {
+  plusSlides(1);
+});
+
+// Change slide every 2.5 seconds
+setInterval(function() {
+  plusSlides(1);
+}, 2500);
