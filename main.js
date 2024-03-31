1f3710ec2b0bcfdcd3e1ee6aea9c09595c8f649b
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-03-31 14:54:16 +0530

    Done

--- main.js
+++ main.js
@@ -2,18 +2,22 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -23,15 +27,15 @@ function showSlides(n) {
 let prevBtn = document.getElementById("prevBtn");
 let nextBtn = document.getElementById("nextBtn");
 
-prevBtn.addEventListener("click", function() {
+prevBtn.addEventListener("click", function () {
   plusSlides(-1);
 });
 
-nextBtn.addEventListener("click", function() {
+nextBtn.addEventListener("click", function () {
   plusSlides(1);
 });
 
 // Change slide every 2.5 seconds
-setInterval(function() {
+setInterval(function () {
   plusSlides(1);
 }, 2500);
